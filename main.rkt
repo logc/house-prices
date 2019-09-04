8404a28acf08bf066d895efafc931c69890b155e
Luis Osa <luis.osa.gdc@gmail.com>
2019-09-04 19:26:55 +0200

    Add table data structure

--- main.rkt
+++ main.rkt
@@ -2,5 +2,16 @@
 
 (require (prefix-in io: "io.rkt"))
 
+(struct table (rows cols shape))
+(define (vecvec->table vecvec)
+  (define num-rows (vector-length vecvec))
+  (define num-cols (vector-length (vector-ref vecvec 0)))
+  (define rows (for/hash ([i (in-naturals)] [vec vecvec])
+                 (values i vec)))
+  (define cols (for/hash ([j (in-range num-cols)])
+                 (values j (for/vector ([i (in-range num-rows)])
+                             (vector-ref (vector-ref vecvec i) j)))))
+  (table rows cols (vector num-rows num-cols)))
+
 (module+ main
-  (define M (io:parse-file "data/train.csv")))
+  (define S (vecvec->table (io:parse-file "data/train.csv"))))
